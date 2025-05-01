e318deacd1799293b6d378df0ebcf3bd42d564f7
Muying Zhao <muying.zhao31@gmail.com>
2025-05-01 15:55:21 +0100

    Update sidemap.ts for new pages routing

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,9 @@
 import { getBlogPosts } from 'app/blog/utils'
+import { getProjectsInfo } from './projects/utils'
+import { getEducationInfo } from './education/utils'
+import { getExperienceInfo } from './experience/utils'
 
-export const baseUrl = 'https://portfolio-blog-starter.vercel.app'
+export const baseUrl = 'https://muyingzhao.co.uk'
 
 export default async function sitemap() {
   let blogs = getBlogPosts().map((post) => ({
@@ -8,10 +11,26 @@ export default async function sitemap() {
     lastModified: post.metadata.publishedAt,
   }))
 
-  let routes = ['', '/blog'].map((route) => ({
+  let projects = getProjectsInfo().map(project => ({
+    url: `${baseUrl}/projects/${project.slug}`,
+    lastModified: project.metadata.period.split('-')[1] // Use end date
+  }))
+
+  let expriences = getExperienceInfo().map(experience => ({
+    url: `${baseUrl}/experience/${experience.slug}`,
+    lastModified: experience.metadata.period.split('-')[1] // Use end date
+  }))
+
+  let education = getEducationInfo().map(education => ({
+    url: `${baseUrl}/education/${education.slug}`,
+    lastModified: education.metadata.period.split('-')[1] // Use end date
+  }))
+
+  let routes = ['', '/blog', '/projects', '/experience', '/education'].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }))
 
-  return [...routes, ...blogs]
+
+  return [...routes, ...blogs, ...projects, ...expriences, ...education]
 }
